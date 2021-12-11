60d25bd44a2219b1211190cc566c50133986d97b
rogexis <rogexis@gmail.com>
2021-12-11 14:07:20 -0600

    Un commit más...

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,10 +1,44 @@
 import React from "react";
-
+import ExerciseForm from "../components/ExerciseForm";
+import Card from "../components/Card";
 
 class ExerciseNew extends React.Component{
+    
+    state = {
+        form: {
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '#f0f0f0f',
+            rightColor: '#0f0f0f'
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
 
+            }
+
+        })
+    }
     render(){
-        return "New Exercise"
+
+
+        return (
+            <div className="row">
+                <div className="col-sm">
+                    <Card {...this.state.form} />
+                </div>
+                <div className="col-sm">
+                    <ExerciseForm 
+                        onChange = {this.handleChange}
+                        form = {this.state.form}
+                    />
+                </div>
+            </div>
+        )
     }
 
 }
